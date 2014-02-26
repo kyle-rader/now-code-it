c497174fbca232077a4cb514b1607f6e80acf909
kyle-rader <kyle.rader@ieee.org>
2014-02-26 08:39:09 -0800

    Add recursive reverse string print.

---
+++ code/C#/reverse-print-string/reverse-print-string/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_print_string
+{
+	class Program
+	{
+		static public void reverse_print(string input, bool first = true)
+		{
+			if (input.Length == 1)
+			{
+				System.Console.Write(input);
+			}
+			else
+			{
+				System.Console.Write(input[input.Length - 1]);
+				reverse_print(input.Substring(0, input.Length - 1), false);
+			}
+			if (first)
+				System.Console.WriteLine();
+		}
+
+		static void Main(string[] args)
+		{
+			string hi = "hello world";
+			reverse_print(hi);
+		}
+	}
+}
